52f690ceb0a7f552687cbf953765335c27cfd28e
laurapyle <laurapyle@gmail.com>
2023-09-09 20:16:47 -0600

    label and write file

--- Ostendorf/randomize clusters.R
+++ Ostendorf/randomize clusters.R
@@ -4,3 +4,9 @@ a <- sample.int(4,4,replace = F)
 # randomize order of clusters 5-8, with 1:1 support
 b <- sample.int(4,4,replace = F)
 b <- b + 4
+
+final <- as.data.frame(cbind(a,b))
+names(final) <- c("without_one_to_one","with_one_to_one")
+
+# write the file
+write.csv(final,"/Volumes/pylell/Ostendorf/K award/Randomization/cluster_randomization.csv", row.names = F)
