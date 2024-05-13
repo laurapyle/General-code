0aaf2aa7dce5dd1f7f5f6b731769d5a370367624
laurapyle <laurapyle@gmail.com>
2024-05-13 14:19:50 -0600

    code colors

--- Hill/randomize clusters.R
+++ Hill/randomize clusters.R
@@ -4,6 +4,9 @@
 a <- sample.int(4,4,replace = F)
 a <- as.data.frame(a)
 names(a) <- c("Cohort")
+a$color <- ifelse(a$Cohort == 1, "Red",
+                 ifelse(a$Cohort == 2, "Yellow",
+                        ifelse(a$Cohort == 3, "Green", "Purple")))
 
 # write the file
 write.csv(a,"/Volumes/pylell/Hill/Randomization/hill_cluster_randomization.csv", row.names = F)
