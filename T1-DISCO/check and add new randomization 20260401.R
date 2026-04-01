74e63bdec2d411e86bd8f9dde6796ff86c3e7f53
laurapyle <laurapyle@gmail.com>
2026-04-01 14:57:47 -0700

    new allocations

--- T1-DISCO/check and add new randomization 20260401.R
+++ T1-DISCO/check and add new randomization 20260401.R
@@ -1,3 +1,30 @@
+library(blockrand)
+library(dplyr)
+
+# check balance of current randomizations
 rand <- read.csv('/Users/pylell/Library/CloudStorage/OneDrive-UW/T1-DISCO randomization/checking randomization 20260401.csv')
 table(rand$Group)
 table(rand$Stratum, rand$Group)
+
+
+set.seed(1234586444)
+
+# create 53 new allocation pairs
+
+# first pair
+rand_first <- blockrand(n=53, levels = c(1:2), block.sizes = 2)
+rand_first$treatment_char <- ifelse(rand_first$treatment == "1", "PLACEBO", "ACTIVE")
+
+rand <- rand_first %>% select(id, treatment_char)
+rand$treatment_char_second <- ifelse(rand$treatment_char == "ACTIVE", "PLACEBO", "ACTIIVE")
+
+# check
+table(rand$treatment_char)
+table(rand$treatment_char_second)
+
+# output
+write.csv(rand, 
+          "/Users/pylell/Library/CloudStorage/OneDrive-UW/T1-DISCO randomization/rand_sequence_BMI_25-35.csv",
+          row.names = F)
+
+
